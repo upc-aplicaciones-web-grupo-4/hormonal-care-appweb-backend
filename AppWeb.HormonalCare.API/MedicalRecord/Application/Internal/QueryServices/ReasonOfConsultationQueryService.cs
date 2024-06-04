a77e48d9cbdf81f6d2b53f631297a93652fa270b
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-06-03 22:56:57 -0500

    feat(ReasonOfConsultationQueryService): added implementation for method that handles getbyid query

---
+++ AppWeb.HormonalCare.API/MedicalRecord/Application/Internal/QueryServices/ReasonOfConsultationQueryService.cs
@@ -0,0 +1,20 @@
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Aggregates;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Queries;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Repositories;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Services;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Application.Internal.QueryServices;
+
+public class ReasonOfConsultationQueryService (IReasonOfConsultationRepository reasonOfConsultationRepository) : IReasonOfConsultationQueryService
+{
+    public async Task<ReasonOfConsultation?> Handle(GetReasonOfConsultationByIdQuery query)
+    {
+        //Caution with this 
+        //Caution with this 
+        //Caution with this 
+        //Caution with this 
+
+        return await reasonOfConsultationRepository.FindByIdAsync(query.Id);
+    }
+}
+
