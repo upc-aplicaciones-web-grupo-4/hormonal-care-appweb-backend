9de4cfd7e208db263c43346359ae739746273fd9
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-06-06 11:52:53 -0500

    feat(medical-record-medication): added entities prescription and medicationType.

---
+++ AppWeb.HormonalCare.API/MedicalRecord/Domain/Model/entities/MedicationType.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Commands;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Entities
+{
+    public class MedicationType
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string TypeName { get; set; }
+
+        public MedicationType() { }
+
+        public MedicationType(CreateMedicationTypeCommand command)
+        {
+            TypeName = command.typeName;
+        }
+
+        public void UpdateTypeName(string typeName)
+        {
+            TypeName = typeName;
+        }
+
+        public string GetName()
+        {
+            return TypeName;
+        }
+
+        public void Update(UpdateMedicationTypeCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppWeb.HormonalCare.API/MedicalRecord/Domain/Model/entities/Prescription.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Commands;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Entities
+{
+    public class Prescription
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        public DateTime PrescriptionDate { get; set; }
+        public string Notes { get; set; }
+
+        public Prescription() { }
+
+        public Prescription(DateTime prescriptionDate, int doctorId, int patientId, string notes)
+        {
+            PrescriptionDate = prescriptionDate;
+            DoctorId = doctorId;
+            PatientId = patientId;
+            Notes = notes;
+        }
+
+        public Prescription(CreatePrescriptionCommand command)
+        {
+            PrescriptionDate = command.prescriptionDate;
+            Notes = command.notes;
+        }
+
+        public void Update(UpdatePrescriptionCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
