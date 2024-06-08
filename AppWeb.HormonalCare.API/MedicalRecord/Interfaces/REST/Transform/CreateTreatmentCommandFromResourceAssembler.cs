6efbd103ea75c2536cca81de4980ea4ecc378175
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-08 17:30:56 -0500

    Feat(treatment Interfaces)
    creating treatment Interfaces

---
+++ AppWeb.HormonalCare.API/MedicalRecord/Interfaces/REST/Transform/CreateTreatmentCommandFromResourceAssembler.cs
@@ -0,0 +1,20 @@
+﻿using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Commands;
+using AppWeb.HormonalCare.API.MedicalRecord.Interfaces.REST.Resources;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Interfaces.REST.Transform;
+
+public class CreateTreatmentCommandFromResourceAssembler
+{
+    public static CreateTreatmentCommand ToCommandFromResource(CreateTreatmentResource resource)
+    {
+        return new CreateTreatmentCommand(resource.Description);
+    }
+}
+
+/*
+
+public static CreateReasonOfConsultationCommand ToCommandFromResource(CreateReasonOfConsultationResource resource)
+    {
+        return new CreateReasonOfConsultationCommand(resource.Description, resource.Symptoms);
+    }
+*/
---
+++ AppWeb.HormonalCare.API/MedicalRecord/Interfaces/REST/Transform/UpdateTreatmentCommandFromResourceAssembler.cs
@@ -0,0 +1,14 @@
+﻿using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Commands;
+using AppWeb.HormonalCare.API.MedicalRecord.Interfaces.REST.Resources;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Interfaces.REST.Transform;
+
+public class UpdateTreatmentCommandFromResourceAssembler
+{
+    public static UpdateTreatmentCommand ToCommandFromResource(int id, UpdateTreatmentResource resource)
+    {
+        return new UpdateTreatmentCommand(id, resource.Description);
+    }
+}
+
+
