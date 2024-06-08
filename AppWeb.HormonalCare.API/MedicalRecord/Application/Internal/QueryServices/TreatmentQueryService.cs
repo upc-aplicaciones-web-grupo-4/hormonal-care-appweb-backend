426d9f12bbe620d775de7711c2d13f6def509779
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-08 17:27:53 -0500

    Feat(Treatment Application)
    creating Treatment Application

---
+++ AppWeb.HormonalCare.API/MedicalRecord/Application/Internal/QueryServices/TreatmentQueryService.cs
@@ -0,0 +1,20 @@
+﻿using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Aggregates;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Queries;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Repositories;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Services;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Application.Internal.QueryServices;
+
+public class TreatmentQueryService (ITreatmentRepository treatementRepository) : ITreatmentQueryService
+{
+    public async Task<Treatment?> Handle(GetTreatmentByIdQuery query)
+    {
+        //Caution with this 
+        //Caution with this 
+        //Caution with this 
+        //Caution with this 
+
+        return await treatementRepository.FindByIdAsync(query.Id);
+    }
+}
+
