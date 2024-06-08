9951ea910543466f48d2d01b211a45d74c08fc78
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-06-08 13:23:42 -0500

    feat(Patient): add repositories for Patient

---
+++ AppWeb.HormonalCare.API/MedicalRecord/Interfaces/ACL/PatientsContextFacade.cs
@@ -0,0 +1,23 @@
+﻿using AppWeb.HormonalCare.API.MedicalRecord.Domain.Model.Commands;
+using AppWeb.HormonalCare.API.MedicalRecord.Domain.Services;
+
+namespace AppWeb.HormonalCare.API.MedicalRecord.Interfaces.ACL;
+
+public class PatientsContextFacade(IPatientCommandService patientCommandService, IPatientQueryService patientQueryService)
+{
+    public async Task<int> CreatePatient(string typeofBloodName)
+    {
+        var createPatientCommand = new CreatePatientCommand(typeofBloodName);
+        var patient = await patientCommandService.Handle(createPatientCommand);
+        return patient?.Id ?? 0;
+    }
+    
+    public async Task<int> UpdatePatient(int id, string typeofBloodName)
+    {
+        var updatePatientCommand = new UpdatePatientCommand(id, typeofBloodName);
+        var patient = await patientCommandService.Handle(updatePatientCommand);
+        return patient?.Id ?? 0;
+    }
+}
+
+
