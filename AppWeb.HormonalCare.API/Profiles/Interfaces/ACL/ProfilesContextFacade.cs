f9a6ff8b3ee61c9836c8f833e3bfad2772235cc9
hernancit0 <ernanemiliom12@gmail.com>
2024-06-09 13:54:57 -0500

    fix: delete history clinic
    delete history clinic and upload medicalrecord

--- AppWeb.HormonalCare.API/Profiles/Interfaces/ACL/ProfilesContextFacade.cs
+++ AppWeb.HormonalCare.API/Profiles/Interfaces/ACL/ProfilesContextFacade.cs
@@ -1,65 +1,77 @@
+using System;
+using System.Threading.Tasks;
 using AppWeb.HormonalCare.API.Profiles.Domain.Model.Commands;
 using AppWeb.HormonalCare.API.Profiles.Domain.Model.Queries;
 using AppWeb.HormonalCare.API.Profiles.Domain.Model.Services;
 using AppWeb.HormonalCare.API.Profiles.Domain.Model.ValueObjects;
 
-namespace AppWeb.HormonalCare.API.Profiles.Interfaces.ACL;
-
-/**
- * Profiles context facade.
- *
- * <summary>
- * This class represents the profiles context facade, part of the profiles anti-corruption layer.
- * It contains the methods to interact with the profiles context from other bounded context.
- * </summary>
- */
-public class ProfilesContextFacade(IProfileCommandService profileCommandService, IProfileQueryService profileQueryService)
+namespace AppWeb.HormonalCare.API.Profiles.Interfaces.ACL
 {
     /**
-     * Create a profile.
+     * Profiles context facade.
      *
-     * <param name="firstName">The first name of the profile</param>
-     * <param name="lastName">The last name of the profile</param>
-     * <param name="email">The email of the profile</param>
-     * <param name="street">The street of the profile</param>
-     * <param name="number">The number of the profile</param>
-     * <param name="city">The city of the profile</param>
-     * <param name="postalCode">The postal code of the profile</param>
-     * <param name="country">The country of the profile</param>
-     * <returns>The profile id</returns>
-     * 
+     * <summary>
+     * This class represents the profiles context facade, part of the profiles anti-corruption layer.
+     * It contains the methods to interact with the profiles context from other bounded context.
+     * </summary>
      */
-    public async Task<int> CreateProfile(
-        string firstName, 
-        string lastName, 
-        string image, 
-        string gender, 
-        DateTime birthDate, 
-        string phone, 
-        string email)
+    public class ProfilesContextFacade
     {
-        var createProfileCommand = new CreateProfileCommand(firstName, 
-            lastName, 
-            image, 
-            gender, 
-            birthDate, 
-            phone, 
-            email);
-        var profile = await profileCommandService.Handle(createProfileCommand);
-        return profile?.Id ?? 0;
-    }
-    
-    /**
-     * Fetch a profile id by email.
-     *
-     * <param name="email">The email of the profile</param>
-     * <returns>The profile id</returns>
-     * 
-     */
-    public async Task<int> FetchProfileIdByEmail(string email)
-    {
-        var getProfileByEmailQuery = new GetProfileByEmailQuery(new EmailAddress(email));
-        var profile = await profileQueryService.Handle(getProfileByEmailQuery);
-        return profile?.Id ?? 0;
+        private readonly IProfileCommandService _profileCommandService;
+        private readonly IProfileQueryService _profileQueryService;
+
+        public ProfilesContextFacade(IProfileCommandService profileCommandService, IProfileQueryService profileQueryService)
+        {
+            _profileCommandService = profileCommandService;
+            _profileQueryService = profileQueryService;
+        }
+
+        /**
+         * Create a profile.
+         *
+         * <param name="firstName">The first name of the profile</param>
+         * <param name="lastName">The last name of the profile</param>
+         * <param name="email">The email of the profile</param>
+         * <param name="street">The street of the profile</param>
+         * <param name="number">The number of the profile</param>
+         * <param name="city">The city of the profile</param>
+         * <param name="postalCode">The postal code of the profile</param>
+         * <param name="country">The country of the profile</param>
+         * <returns>The profile id</returns>
+         * 
+         */
+        public async Task<int> CreateProfile(
+            string firstName,
+            string lastName,
+            string image,
+            string gender,
+            DateTime birthDate,
+            string phone,
+            string email)
+        {
+            var createProfileCommand = new CreateProfileCommand(firstName,
+                lastName,
+                image,
+                gender,
+                birthDate,
+                phone,
+                email);
+            var profile = await _profileCommandService.Handle(createProfileCommand);
+            return profile?.Id ?? 0;
+        }
+
+        /**
+         * Fetch a profile id by email.
+         *
+         * <param name="email">The email of the profile</param>
+         * <returns>The profile id</returns>
+         * 
+         */
+        public async Task<int> FetchProfileIdByEmail(string email)
+        {
+            var getProfileByEmailQuery = new GetProfileByEmailQuery(new EmailAddress(email));
+            var profile = await _profileQueryService.Handle(getProfileByEmailQuery);
+            return profile?.Id ?? 0;
+        }
     }
 }
